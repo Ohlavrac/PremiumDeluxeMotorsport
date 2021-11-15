d3e96c64540e8fe7a1361469dad88e581378781a
Danilo Aranha <sraranhagameplays@gmail.com>
2021-11-15 16:04:38 -0300

    VsCode refactored by itself

--- premiun_deluxe_motorsport/lib/modules/vehicles_list_page/vehicles_list_page.dart
+++ premiun_deluxe_motorsport/lib/modules/vehicles_list_page/vehicles_list_page.dart
@@ -7,7 +7,7 @@ import 'package:premiun_deluxe_motorsport/shared/themes/app_text_styles.dart';
 import 'package:premiun_deluxe_motorsport/shared/widgets/input_widget.dart';
 
 class VehiclesListPage extends StatefulWidget {
-  const VehiclesListPage({ Key? key }) : super(key: key);
+  const VehiclesListPage({Key? key}) : super(key: key);
 
   @override
   _VehiclesListPageState createState() => _VehiclesListPageState();
@@ -23,7 +23,10 @@ class _VehiclesListPageState extends State<VehiclesListPage> {
     if (value.isEmpty) {
       results = listVehicles;
     } else {
-      results = listVehicles.where((vehicle) => vehicle.name.toLowerCase().contains(value.toLowerCase())).toList();
+      results = listVehicles
+          .where((vehicle) =>
+              vehicle.name.toLowerCase().contains(value.toLowerCase()))
+          .toList();
     }
 
     setState(() {
@@ -41,116 +44,120 @@ class _VehiclesListPageState extends State<VehiclesListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.background,
-      appBar: AppBar(
-        backgroundColor: AppColors.menuButtom,
-        title: Image.asset('images/logo.png'),
-        centerTitle: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 10, right: 10),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 15, bottom: 15),
-              child: TextField(
-                onChanged: (value) {
-                  _runSearch(value);
-                },
-                decoration: InputDecoration(
-                  label: Text(
-                    "Procurar",
-                    style: TextStyles.titleInput,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.white)
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.white)
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.white)
-                  ),
-                  suffix: Icon(Icons.search, color: AppColors.secondary,),
-                ),
-                style: TextStyles.titleInput,
-              ),
-            ),
-            Expanded(
-              child: searchVehicle.isNotEmpty 
-              ? ListView.builder(
-                itemBuilder: (BuildContext context, int vehicle) {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 360,
-                          height: 170,
-                          decoration: BoxDecoration(color: AppColors.menuButtom, borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)), 
-                            image: DecorationImage(
-                              image: NetworkImage(searchVehicle[vehicle].image),
-                              fit: BoxFit.cover
-                            ),
-                          ), 
-                        ),
-                        Container(
-                          width: 360,
-                          height: 75,
-                          
-                          decoration: BoxDecoration(
-                            color: AppColors.menuButtom,
-                            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      //list_vehicles[vehicle].name,
-                                      searchVehicle[vehicle].name,
-                                      style: TextStyles.titleComprovante,
-                                    ),
-                                    Text(
-                                      "${searchVehicle[vehicle].capacity.toString()} Kg",
-                                      style: TextStyles.titleComprovante,
-                                    ),
-                                    
-                                  ],
-                                ),
-                                Text(
-                                  "${searchVehicle[vehicle].price.toString()} \$",
-                                  style: TextStyles.titleComprovante,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                //separatorBuilder: (_, ____) => Divider(),
-                itemCount: searchVehicle.length
-              )
-              : Text(
-                "Carro não encontrado",
-                style: TextStyles.titleInput
-              )
-            ),
-          ],
+        backgroundColor: AppColors.background,
+        appBar: AppBar(
+          backgroundColor: AppColors.menuButtom,
+          title: Image.asset('images/logo.png'),
+          centerTitle: true,
         ),
-      )
-    );
-
+        body: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 15),
+                child: TextField(
+                  onChanged: (value) {
+                    _runSearch(value);
+                  },
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Procurar",
+                      style: TextStyles.titleInput,
+                    ),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(color: Colors.white)),
+                    suffix: Icon(
+                      Icons.search,
+                      color: AppColors.secondary,
+                    ),
+                  ),
+                  style: TextStyles.titleInput,
+                ),
+              ),
+              Expanded(
+                  child: searchVehicle.isNotEmpty
+                      ? ListView.builder(
+                          itemBuilder: (BuildContext context, int vehicle) {
+                            return Padding(
+                              padding: const EdgeInsets.only(top: 30),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    width: 360,
+                                    height: 170,
+                                    decoration: BoxDecoration(
+                                      color: AppColors.menuButtom,
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(10),
+                                          topRight: Radius.circular(10)),
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              searchVehicle[vehicle].image),
+                                          fit: BoxFit.cover),
+                                    ),
+                                  ),
+                                  Container(
+                                    width: 360,
+                                    height: 75,
+                                    decoration: BoxDecoration(
+                                      color: AppColors.menuButtom,
+                                      borderRadius: BorderRadius.only(
+                                          bottomLeft: Radius.circular(10),
+                                          bottomRight: Radius.circular(10)),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                //list_vehicles[vehicle].name,
+                                                searchVehicle[vehicle].name,
+                                                style:
+                                                    TextStyles.titleComprovante,
+                                              ),
+                                              Text(
+                                                "${searchVehicle[vehicle].capacity.toString()} Kg",
+                                                style:
+                                                    TextStyles.titleComprovante,
+                                              ),
+                                            ],
+                                          ),
+                                          Text(
+                                            "${searchVehicle[vehicle].price.toString()} \$",
+                                            style: TextStyles.titleComprovante,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                          //separatorBuilder: (_, ____) => Divider(),
+                          itemCount: searchVehicle.length)
+                      : Text("Carro não encontrado",
+                          style: TextStyles.titleInput)),
+            ],
+          ),
+        ));
   }
 }
